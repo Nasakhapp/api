8b454aeeb2c575f0366cb5bae05c5424dc67c4fd
oonjib <backdev.oonjib@gmail.com>
2024-06-30 04:49:09 +0330

    hamkam init

--- index.ts
+++ index.ts
@@ -56,9 +56,7 @@ if (process.env.NODE_ENV === "production") app.use(bot.webhookCallback("/"));
 // var cert = fs.readFileSync(__dirname + "/certs/selfsigned.crt");
 
 const server = http.createServer(app);
-const peerServer = ExpressPeerServer(server, { path: "/" });
-
-app.use("/peerjs", peerServer);
+const peerServer = ExpressPeerServer(server, { path: "/peerjs" });
 
 const socketServer = new io.Server(server, {
   cors: { origin: "*" },
