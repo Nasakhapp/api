68d123356aaac8e503bf8af5fb14389dfb90c640
oonjib <backdev.oonjib@gmail.com>
2024-07-04 18:46:43 +0330

    fix call

--- index.ts
+++ index.ts
@@ -54,6 +54,9 @@ if (process.env.NODE_ENV === "production") app.use(bot.webhookCallback("/"));
 // var cert = fs.readFileSync(__dirname + "/certs/selfsigned.crt");
 
 const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, { path: "/" });
+
+app.use("/peerjs", peerServer);
 
 const socketServer = new io.Server(server, {
   cors: { origin: "*" },
